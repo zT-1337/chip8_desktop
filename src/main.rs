8e27b43a498bbe4b8895cc3cb736e9704964ae41
Thomas Zerr <thomas.zerr.3@hof-university.de>
2022-04-14 20:18:59 +0200

    formatted

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use chip8_core::*;
-use sdl2::EventPump;
-use std::env;
-use std::fs::File;
-use std::io::Read;
 use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
-use sdl2::keyboard::Keycode;
+use sdl2::EventPump;
+use std::env;
+use std::fs::File;
+use std::io::Read;
 
 const SCALE: u32 = 15;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
@@ -26,24 +26,32 @@ fn main() {
 
     let (mut canvas, mut event_pump) = setup_window();
     let mut chip8_game = start_game(&args[1]);
-    
+
     'gameloop: loop {
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit{..} | Event::KeyDown{keycode: Some(Keycode::Escape), ..} => {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
                     break 'gameloop;
-                },
-                Event::KeyDown{keycode: Some(key), ..} => {
+                }
+                Event::KeyDown {
+                    keycode: Some(key), ..
+                } => {
                     if let Some(k) = keycode_to_index(key) {
                         chip8_game.set_key_press(k, true);
                     }
-                },
-                Event::KeyUp{keycode: Some(key), ..} => {
+                }
+                Event::KeyUp {
+                    keycode: Some(key), ..
+                } => {
                     if let Some(k) = keycode_to_index(key) {
                         chip8_game.set_key_press(k, false);
                     }
-                },
-                _ => ()
+                }
+                _ => (),
             }
         }
 
@@ -113,22 +121,22 @@ fn draw_screen(emu: &Emulator, canvas: &mut Canvas<Window>) {
 
 fn keycode_to_index(key: Keycode) -> Option<usize> {
     match key {
-        Keycode::Num1 =>    Some(0x1),
-        Keycode::Num2 =>    Some(0x2),
-        Keycode::Num3 =>    Some(0x3),
-        Keycode::Num4 =>    Some(0xC),
-        Keycode::Q =>       Some(0x4),
-        Keycode::W =>       Some(0x5),
-        Keycode::E =>       Some(0x6),
-        Keycode::R =>       Some(0xD),
-        Keycode::A =>       Some(0x7),
-        Keycode::S =>       Some(0x8),
-        Keycode::D =>       Some(0x9),
-        Keycode::F =>       Some(0xE),
-        Keycode::Z =>       Some(0xA),
-        Keycode::X =>       Some(0x0),
-        Keycode::C =>       Some(0xB),
-        Keycode::V =>       Some(0xF),
-        _ =>                None,
+        Keycode::Num1 => Some(0x1),
+        Keycode::Num2 => Some(0x2),
+        Keycode::Num3 => Some(0x3),
+        Keycode::Num4 => Some(0xC),
+        Keycode::Q => Some(0x4),
+        Keycode::W => Some(0x5),
+        Keycode::E => Some(0x6),
+        Keycode::R => Some(0xD),
+        Keycode::A => Some(0x7),
+        Keycode::S => Some(0x8),
+        Keycode::D => Some(0x9),
+        Keycode::F => Some(0xE),
+        Keycode::Z => Some(0xA),
+        Keycode::X => Some(0x0),
+        Keycode::C => Some(0xB),
+        Keycode::V => Some(0xF),
+        _ => None,
     }
 }
