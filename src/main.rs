99d7dd9e5a21ce143415acb20a27fb4d23d7c776
Thomas Zerr <thomas.zerr.3@hof-university.de>
2022-04-14 19:39:16 +0200

    -added: frontend logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,122 @@
+use chip8_core::*;
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use sdl2::event::Event;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+use sdl2::keyboard::Keycode;
+
+const SCALE: u32 = 15;
+const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
+const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
+
+const CYCLES_PER_FRAME: usize = 10;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("Usage: program_name path/to/game");
+        return;
+    }
+
+    //Setup SDL
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("CHIP-8 Emulator", WINDOW_WIDTH, WINDOW_HEIGHT)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let mut chip8 = Emulator::new();
+
+    let mut rom_file = File::open(&args[1]).expect("Unable to open file");
+    let mut rom_content = Vec::new();
+    rom_file.read_to_end(&mut rom_content).unwrap();
+
+    chip8.load_rom(&rom_content);
+
+    'gameloop: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit{..} | Event::KeyDown{keycode: Some(Keycode::Escape), ..} => {
+                    break 'gameloop;
+                },
+                Event::KeyDown{keycode: Some(key), ..} => {
+                    if let Some(k) = key_to_button(key) {
+                        chip8.set_key_press(k, true);
+                    }
+                },
+                Event::KeyUp{keycode: Some(key), ..} => {
+                    if let Some(k) = key_to_button(key) {
+                        chip8.set_key_press(k, false);
+                    }
+                },
+                _ => ()
+            }
+        }
+
+        for _ in 0..CYCLES_PER_FRAME {
+            chip8.cycle();
+        }
+
+        chip8.tick_timers();
+        draw_screen(&chip8, &mut canvas);
+    }
+}
+
+fn draw_screen(emu: &Emulator, canvas: &mut Canvas<Window>) {
+    //Clear Canvas
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+
+    let screen = emu.get_display();
+
+    //Draw each pixel of the screen as white
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    for (i, pixel) in screen.iter().enumerate() {
+        if *pixel {
+            let x = (i % SCREEN_WIDTH) as u32;
+            let y = (i / SCREEN_WIDTH) as u32;
+
+            //Draw pixel as rectangle
+            let rect = Rect::new((x * SCALE) as i32, (y * SCALE) as i32, SCALE, SCALE);
+            canvas.fill_rect(rect).unwrap();
+        }
+    }
+
+    canvas.present();
+}
+
+fn key_to_button(key: Keycode) -> Option<usize> {
+    match key {
+        Keycode::Num1 =>    Some(0x1),
+        Keycode::Num2 =>    Some(0x2),
+        Keycode::Num3 =>    Some(0x3),
+        Keycode::Num4 =>    Some(0xC),
+        Keycode::Q =>       Some(0x4),
+        Keycode::W =>       Some(0x5),
+        Keycode::E =>       Some(0x6),
+        Keycode::R =>       Some(0xD),
+        Keycode::A =>       Some(0x7),
+        Keycode::S =>       Some(0x8),
+        Keycode::D =>       Some(0x9),
+        Keycode::F =>       Some(0xE),
+        Keycode::Z =>       Some(0xA),
+        Keycode::X =>       Some(0x0),
+        Keycode::C =>       Some(0xB),
+        Keycode::V =>       Some(0xF),
+        _ =>                None,
+    }
 }
